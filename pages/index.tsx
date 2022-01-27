0988ef155a67b821ac228977e9b83d11db87b9d0
TomKim <babooteng2@gmail.com>
2022-01-28 01:01:15 +0900

    4.9 Responsive Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {  
   return (
     <div className='bg-slate-400 py-20 px-20 grid gap-10 min-h-screen'>
-      <div className='bg-white p-6 rounded-3xl shadow-xl'>
+      <div className='bg-white sm:bg-red-400 sm:hover:bg-pink-500 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-400 p-6 rounded-3xl shadow-xl'>
         <span className='font-semibold text-3xl'>Select Item</span>
         <ul>
 					{[0, 1, 2, 3, 4, 5].map(i => 
@@ -125,6 +125,7 @@ const Home: NextPage = () => {
 					file:hover:border
 					file:transition-colors' />
 			</div>
+			
 			<div className='bg-white p-10 rounded-2xl shadow-xl'>
 				<p className='first-letter:capitalize first-letter:hover:text-purple-400'>lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, ex, minima alias ipsam molestiae inventore voluptas odit atque explicabo nostrum perspiciatis praesentium ratione nisi, dolorem iure quae? At, nulla mollitia?</p>
 			</div>
