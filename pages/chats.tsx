e773e9c2ae6ff65a03a1b7f2ee72bafd6596490b
TomKim <babooteng2@gmail.com>
2022-02-27 17:58:49 +0900

    5.8 pre Chats

---
+++ pages/chats.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+
+const Chats: NextPage = () => {
+  return (
+    <div>      
+        <div>
+          <div />
+          <div>
+            <p>Steve Jebs</p>
+            <p>
+              See you tomorrow in the corner at 2pm!
+            </p>
+          </div>
+        </div>      
+    </div>
+  );
+};
+
+export default Chats;
