96c54711b10d9649816904cb6804e3640d8d5b42
TomKim <babooteng2@gmail.com>
2022-06-12 16:54:53 +0900

    10.2 useUser Hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,17 @@ import type { NextPage } from 'next'
 import FloatingButton from '@components/floating-button';
 import Item from '@components/item';
 import Layout from '@components/layout';
+import useUser from '@libs/client/useUser';
+import Head from 'next/head';
 
 //https://dribbble.com/shots/15996385-Ecommerce-elements
 
 const Home: NextPage = () => {  
+  const user = useUser();
+  console.log( "user : ", user)
   return (
     <Layout title="홈" hasTabBar>
+      <Head><title>Home</title></Head>
       <div className='flex flex-col space-y-5 divide-y'>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, i) => (          
           <Item 
