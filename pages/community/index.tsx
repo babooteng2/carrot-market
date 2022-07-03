06dd5fa17de3376fbfdde3aaf28ab84157ec759e
TomKim <babooteng2@gmail.com>
2022-07-03 17:00:20 +0900

    end of Authentication , update package - react version up from 18rc to 18.2

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import NextPage from "next";
 import Link from "next/link";
 import FloatingButton from "@components/floating-button";
 import Layout from "@components/layout";
