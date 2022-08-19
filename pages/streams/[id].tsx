797aa54c1f4b489bf57e10ddd525c1c6ad0101df
TomKim <babooteng2@gmail.com>
2022-08-20 00:45:47 +0900

    14.2 Send Message

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -4,7 +4,12 @@ import Message from "@components/message";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { Stream } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
 
+interface IMessageForm {
+  message: string;
+}
 interface IStreamResponse {
   ok: boolean;
   stream: Stream;
@@ -12,9 +17,18 @@ interface IStreamResponse {
 
 const StreamDetail: NextPage = () => {
   const router = useRouter();
+  const { register, handleSubmit, reset } = useForm<IMessageForm>();
   const { data } = useSWR<IStreamResponse>(
     router.query.id ? `/api/streams/${router.query.id}` : null,
   );
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`,
+  );
+  const onValid = (form: IMessageForm) => {
+    if (loading) return;
+    reset();
+    sendMessage(form);
+  };
   return (
     <Layout canGoBack>
       <div className="py-10 px-4 space-y-4">
@@ -31,11 +45,12 @@ const StreamDetail: NextPage = () => {
         <div>
           <h2 className="text-gray-900 font-bold text-2xl">Live Chat</h2>
           <div className="pt-10 pb-16 px-4 space-y-4 h-[50vh] overflow-y-auto">
+            {/* 
             <Message
               message="Hi how much are you selling them for? lorem Hi how much are you selling them for?
           Hi how much are you selling them for?Hi how much are you selling them for?Hi how much are you selling them for?"
               reverse
-            />
+            />            
             <Message message="Hi how much are you selling them for?" />
             <Message message="I want ￦20,000" reverse />
             <Message message="no, never stop!!" />
@@ -44,22 +59,30 @@ const StreamDetail: NextPage = () => {
             <Message message="I want ￦20,000" reverse />
             <Message message="no, never stop!!" />
             <Message message="Hi how much are you selling them for?" />
+            <Message message="I want ￦20,000" reverse />
+            <Message message="no, never stop!!" />
+             */}
+
             <Message message="I want ￦20,000" reverse />
             <Message message="no, never stop!!" />
           </div>
         </div>
         <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
-          <div className="flex relative items-center">
+          <form
+            className="flex relative items-center"
+            onSubmit={handleSubmit(onValid)}
+          >
             <input
+              {...register("message", { required: true })}
               type="text"
               className="shadow-sm rounded-full w-full border-gray-300 pr-12 focus:ring-orange-500 focus:outline-none focus:border-orange-500"
             />
             <div className="absolute inset-y-0 flex py-1 pr-1.5 right-0">
               <button className="flex focus:ring-2 focus:ring-offset-2 items-center focus:ring-orange-500 focus:outline-none bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600">
-                &rarr;
+                {loading ? "Sending..." : null} &rarr;
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </Layout>
